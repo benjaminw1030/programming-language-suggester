7a5e68ee4a69ba9b4dc667140334d01c13e0182a
Ben Wilson <benjaminw1030@gmail.com>
2021-08-13 11:47:02 -0700

    add values to questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("#suggester").submit(function(event) {
+$(document).ready(function () {
+  $("#suggester").submit(function (event) {
     event.preventDefault();
     const name = $("#name").val();
     const q1 = $("input:radio[name=reason]:checked").val();
@@ -8,11 +8,14 @@ $(document).ready(function() {
     const q4 = $("input:radio[name=reason]:checked").val();
     const q5 = $("input:radio[name=reason]:checked").val();
     let cplusplus = 0;
+    let csharp = 0
     let ruby = 0;
     let python = 0;
     let java = 0;
-    let c = 0;
+    let objectivec = 0;
     let javascript = 0;
+
+    // Hides/shows name alert and result
 
     if (!name) {
       $("#nameAlert").addClass("visible");
@@ -28,53 +31,88 @@ $(document).ready(function() {
       $("#nameAlert").addClass("visible");
     };
 
+    // Branching questions increment value to language variables above
+
     if (q1 === "fun") {
-      python++;
+      python += 2;
+      ruby += 1;
     } else if (q1 === "improve") {
-
+      cplusplus += 2;
     } else if (q1 === "money") {
-
-    } else if (q1 ==="dunno") {
-      python++;
+      python += 1;
+      javascript += 1;
+      objectivec += 1
+      csharp += 1;
+      java += 1;
+    } else if (q1 === "dunno") {
+      python += 1;
     }
 
     if (q2 === "bigTech") {
-
+      csharp += 2;
+      objectivec += 2;
+      python += 1;
     } else if (q2 === "web") {
-
+      javascript += 3;
+      java += 1;
+      ruby += 2;
     } else if (q2 === "mobile") {
-
+      objectivec += 2;
+      java += 2;
     } else if (q2 === "gaming") {
-
-    } else if (q2 ==="dunno") {
-      python++;
+      cplusplus += 2;
+      csharp += 2;
+    } else if (q2 === "dunno") {
+      python += 1;
     }
 
     if (q3 === "none") {
-
+      ruby += 1;
+      python += 2;
+      javascript += 1;
     } else if (q3 === "little") {
-
+      ruby += 1;
+      csharp += 1;
+      java += 1;
     } else if (q3 === "pro") {
-
+      cplusplus += 1;
     }
 
     if (q4 === "apple") {
-
+      python += 1;
+      cplusplus += 1;
+      objectivec += 2;
+      csharp -= 1;
     } else if (q4 === "microsoft") {
-
+      javascript += 1;
+      java += 1;
+      csharp += 1;
+      ruby += 1;
+      objectivec -= 1;
     } else if (q4 === "linux") {
-
+      cplusplus += 1;
+      python += 1;
+      java += 1;
+      javascript += 1;
     }
 
-    if (q5 === "yes") {
-
-    } else if (q4 === "no") {
-
-    } else if (q4 === "meh") {
-
+    if (q5 === "startup") {
+      javascript += 1;
+      ruby += 1;
+    } else if (q4 === "corporation") {
+      cplusplus += 1;
+      csharp += 1;
+      java += 2;
+      objectivec += 1;
+    } else if (q4 === "myself") {
+      python += 2;
     }
 
-  });  
+// Comparing values
+
+    if (cplusplus 
+
+  });
 });
 
-// languages: C++, python, ruby, 
+
