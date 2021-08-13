8d1b936cf7b890eff7b77ba86989f70352677fe5
Ben Wilson <benjaminw1030@gmail.com>
2021-08-13 09:41:21 -0700

    add name alert fade in functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-  $("#quiz").submit(function(event) {
+  $("#suggester").submit(function(event) {
     event.preventDefault();
+    const name = $("#name").val();
+    const q1 = $("input:radio[name=reason]:checked").val();
+    const q2 = $("input:radio[name=reason]:checked").val();
+    const q3 = $("input:radio[name=reason]:checked").val();
+    const q4 = $("input:radio[name=reason]:checked").val();
+    const q5 = $("input:radio[name=reason]:checked").val();
 
+    if (!name) {
+      $("#nameAlert").addClass("visible");
+      $("#nameAlert").fadeIn();
+      
+    }
   });
 });
